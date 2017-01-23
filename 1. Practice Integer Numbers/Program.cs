246f000699e7f36cfbdb28bf65538902063c4346
Margi Hadzhieva <margarita.hadjieva@mail.bg>
2017-01-23 14:59:18 +0200

    programming fundamentals

---
+++ 1. Practice Integer Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Practice_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             sbyte exerciseNumber1 = -100;
+        byte exerciseNumber2 = 128;
+        short exerciseNumber3 = -3540;
+        ushort exerciseNumber4 = 64876;
+        uint exerciseNumber5 = 2147483648;
+        int exerciseNumber6 = -1141583228;
+        long exerciseNumber7 = -1223372036854775808;
+        Console.WriteLine(exerciseNumber1);
+        Console.WriteLine(exerciseNumber2);
+        Console.WriteLine(exerciseNumber3);
+        Console.WriteLine(exerciseNumber4);
+        Console.WriteLine(exerciseNumber5);
+        Console.WriteLine(exerciseNumber6);
+        Console.WriteLine(exerciseNumber7);
+ 
+        }
+    }
+}
